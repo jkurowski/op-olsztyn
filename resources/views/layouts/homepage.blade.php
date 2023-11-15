ce0872bce1c1b44c04c7fbcf6601e5bfb80cad4a
Jacek <kontakt@jk-design.pl>
2023-11-15 15:00:32 +0100

    RWD - cz. 2

--- resources/views/layouts/homepage.blade.php
+++ resources/views/layouts/homepage.blade.php
@@ -47,11 +47,11 @@
     </ul>
 </div>
 
-<section class="">
+<section class="rwd-section">
     <div class="container">
         <div class="row">
-            <div class="col-7">
-                <div class="pe-5">
+            <div class="col-12 col-lg-7">
+                <div class="pe-0 pe-lg-5">
                     <img src="{{ asset('/images/svg/header-deco.svg') }}" alt="" width="99" height="17" class="mb-3">
                     <h2>Poczuj prywatność i komfort w kameralnym budynku na Wyzwolenia</h2>
                     <p><b>W ścisłym centrum blisko Starego Miasta Olsztyn powstają wysokiej klasy apartamenty w jednym, kameralnym budynku. To niezwykły projekt w segmencie premium. Wyjątkowe miejsce, inne od klasycznego nowego osiedla od dewelopera.</b></p>
@@ -59,10 +59,10 @@
                     <p>Apartamenty Wyzwolenia zostały zaprojektowane z myślą o osobach, które marzą o  nowym mieszkaniu w Olsztynie, cenią mądre rozwiązania, szukają oryginalności oraz jakości wykonania, wykorzystają dostęp do pełnej infrastruktury i lubią żyć w centrum miasta.</p>
                     <p>&nbsp;</p>
                     <p>Twoimi bezpośrednimi sąsiadami będą jedynie mieszkańcy kamienic. Takie komfortowe poczucie prywatności i wyciszenia w centralnej części Olsztyna jest już właściwie niespotykane w ramach nowych projektów deweloperskich.</p>
-                    <a href="" class="bttn mt-5">Kontakt z nami</a>
+                    <a href="{{ route('contact.index') }}" class="bttn mt-4 mt-md-5">Kontakt z nami</a>
                 </div>
             </div>
-            <div class="col-5 d-flex align-items-center">
+            <div class="col-12 col-lg-5 d-none d-lg-flex align-items-center mt-5 mt-lg-0">
                 <div>
                     <img src="{{ asset('/images/investment.jpg') }}" alt="" width="770" height="770">
                 </div>
@@ -71,29 +71,29 @@
     </div>
 </section>
 
-<section class="pt-0">
+<section class="pt-0 rwd-section">
     <div class="container">
         <div class="row d-flex justify-content-center">
-            <div class="col-9">
+            <div class="col-12 col-lg-9">
                 <img src="{{ asset('/images/homepage-investment.jpg') }}" alt="" width="990" height="557">
             </div>
         </div>
 
-        <div class="row pt-5">
+        <div class="row pt-4 pt-md-5">
             <div class="col-12 text-center">
                 <img src="{{ asset('/images/svg/header-deco.svg') }}" alt="" width="99" height="17" class="m-auto mb-3">
                 <h2>Inwestycja – idealna dla Ciebie</h2>
             </div>
-            <div class="col-6">
+            <div class="col-12 col-md-6">
                 <p><b>Projekt zakłada budowę 7-piętrowego budynku o ciekawej, nowoczesnej kaskadowej bryle. W budynku powstaną łącznie 104 apartamenty oraz 2 lokale o charakterze usługowym. Każdy apartament dysponować będzie przestrzennym balkonem/loggią  do 16 mkw, tarasem do 90 mkw lub ogródkiem o powierzchni do 58 mkw. W inwestycji znajdować się będą komórki lokatorskie do 7mkw. Ceny apartamentów są uzależnione od kondygnacji, na której będą się znajdowały.</b></p>
                 <p>&nbsp;</p>
                 <p>Stylowe wykończenia części wspólnych z zastosowaniem naturalnego kamienia, oraz dekoracyjnych lameli ściennych – wprowadzają nowoczesność, funkcjonalność i komfort. Czystość formy, duże lustra oraz stonowane barwy dodadzą wnętrzom przestrzeni i światła.</p>
             </div>
-            <div class="col-6">
+            <div class="col-12 col-md-6">
                 <p>Ponadstandardowa wysokość mieszkań i duże przeszklenia podniosą komfort użytkowania lokali. Przewidzieliśmy także miejsce rekreacji dla dorosłych i dzieci. Przyszli mieszkańcy budynku będą mogli korzystać z prywatnej strefy fitness oraz wewnętrznego kids playroomu.</p>
                 <p>&nbsp;</p>
                 <p>Miejsca parkingowe zaplanowano w dwukondygnacyjnej podziemnej hali garażowej, wyposażonej w instalację do ładowania samochodów elektrycznych. Cały teren osiedla będzie monitorowany.</p>
-                <a href="" class="bttn mt-5">Zobacz więcej</a>
+                <a href="{{ route('investment') }}" class="bttn mt-4 mt-md-5">Zobacz więcej</a>
             </div>
         </div>
     </div>
@@ -202,7 +202,7 @@
     </div>
 </section>
 
-<section id="circle-image">
+<section id="circle-image" class="rwd-section">
     <div class="container">
         <div class="row">
             <div class="col-12 text-center">
@@ -212,13 +212,13 @@
         </div>
 
         <div class="row justify-content-center">
-            <div class="col-8">
+            <div class="col-12 col-lg-8">
                 <img src="{{ asset("/images/atuty-osiedla.jpg") }}" alt="" width="906" height="464">
             </div>
         </div>
 
         <div class="row circle-overlay">
-            <div class="col-2 p-0">
+            <div class="col-6 col-sm-4 col-lg-2 p-0">
                 <div class="circle-box">
                     <div class="circle-box-icon">
                         <img src="{{ asset('/images/icons/icon-taras.png') }}" alt="" width="85" height="85">
@@ -226,7 +226,7 @@
                     <p>Przestrzenne balkony/loggie, tarasy i ogródki</p>
                 </div>
             </div>
-            <div class="col-2 p-0 mt-280">
+            <div class="col-6 col-sm-4 col-lg-2 p-0 mt-280">
                 <div class="circle-box">
                     <div class="circle-box-icon">
                         <img src="{{ asset('/images/icons/icon-paint.png') }}" alt="" width="85" height="85">
@@ -234,7 +234,7 @@
                     <p>Ściany malowane <br>na biało</p>
                 </div>
             </div>
-            <div class="col-2 p-0 mt-430">
+            <div class="col-6 col-sm-4 col-lg-2 p-0 mt-430">
                 <div class="circle-box">
                     <div class="circle-box-icon">
                         <img src="{{ asset('/images/icons/icon-door.png') }}" alt="" width="85" height="85">
@@ -242,7 +242,7 @@
                     <p>Drzwi <br>antywłamaniowe</p>
                 </div>
             </div>
-            <div class="col-2 p-0 mt-430">
+            <div class="col-6 col-sm-4 col-lg-2 p-0 mt-430">
                 <div class="circle-box">
                     <div class="circle-box-icon">
                         <img src="{{ asset('/images/icons/icon-elevator.png') }}" alt="" width="85" height="85">
@@ -250,7 +250,7 @@
                     <p>Wysokość mieszkań <br>od 2,6m do 3m</p>
                 </div>
             </div>
-            <div class="col-2 p-0 mt-280">
+            <div class="col-6 col-sm-4 col-lg-2 p-0 mt-280">
                 <div class="circle-box">
                     <div class="circle-box-icon">
                         <img src="{{ asset('/images/icons/icon-lightbulb.png') }}" alt="" width="85" height="85">
@@ -258,7 +258,7 @@
                     <p>Oświetlenie <br>na balkonach</p>
                 </div>
             </div>
-            <div class="col-2 p-0">
+            <div class="col-6 col-sm-4 col-lg-2 p-0">
                 <div class="circle-box">
                     <div class="circle-box-icon">
                         <img src="{{ asset('/images/icons/icon-balcony.png') }}" alt="" width="85" height="85">
@@ -270,19 +270,19 @@
     </div>
 </section>
 
-<section class="pt-0">
+<section class="pt-0 rwd-section">
     <div class="container">
         <div class="row pt-5">
             <div class="col-12 text-center">
                 <img src="{{ asset('/images/svg/header-deco.svg') }}" alt="" width="99" height="17" class="m-auto mb-3">
                 <h2>Apartamenty Wyzwolenia lepszego adresu w Olsztynie nie znajdziesz</h2>
             </div>
-            <div class="col-6">
+            <div class="col-12 col-md-6">
                 <p><b>Apartamenty Wyzwolenia znajdują się zaledwie 10 minut od wszystkiego, czego potrzebujesz. Na wyciągnięcie ręki masz pracę, naukę, handel, naturę, rozrywkę i kulturę.</b></p>
                 <p>&nbsp;</p>
                 <p>Wystarczy tu przyjechać, rozejrzeć się po okolicy, aby podjąć decyzje o zamieszkaniu pod tym adresem. By móc na co dzień spacerować lub biegać po największym lesie miejskim w Olsztynie lub udać się na przejażdżkę rowerową po najbardziej zielonej okolicy. Las Miejski oddalony jest zaledwie 15 minut piechotą od naszej inwestycji, a jego powierzchnia liczy łącznie ponad 1400 ha, z czego główny kompleks – około 1100 ha. Jest największym w Europie kompleksem leśnym położonym w granicach miasta.</p>
             </div>
-            <div class="col-6">
+            <div class="col-12 col-md-6">
                 <p>Jednocześnie ta niezwykła lokalizacja jest świetnie skomunikowana z każdą inną częścią miasta. W promieniu kilkuset metrów dostępne są: szkoły, przedszkola, urzędy, galeria handlowa, przychodnia, czy kino. Nieopodal inwestycji mieści się kilka sklepów spożywczych – najbliższy jedynie 130 m od budynku. W pobliżu znajduje się także przychodnia i apteka.</p>
                 <p>&nbsp;</p>
                 <p>Przystanki komunikacji miejskiej w odległości 4 minut. Natomiast 5 minut spacerem dotrzesz do Starego Miasta i terenów zielonych wokół Zamku Kapituły Warmińskiej. Znajdują się tam zabytki architektoniczne, plac zabaw, fontanna i amfiteatr. Po drugiej stronie Rynku Starego Miasta zlokalizowany jest park Centralny, gdzie również można wypocząć blisko przyrody. Spacer tam zajmie jedyne 10 minut.</p>
@@ -389,7 +389,7 @@
     </div>
 </section>
 
-<section class="dark-blue-bg investement-bg">
+<section class="dark-blue-bg investement-bg rwd-section">
     <div class="container">
         <div class="row justify-content-center pb-5">
             <div class="col-12 text-center">
@@ -401,7 +401,7 @@
                 <p><b>Nowe mieszkanie w centrum Olsztyna</b></p>
             </div>
 
-            <div class="col-8 text-center">
+            <div class="col-12 col-md-8 text-center">
                 <p>Nie możesz się doczekać zakupu nowego mieszkania? Doskonale to rozumiemy – własne mieszkanie daje poczucie bezpieczeństwa i stabilizacji oraz możliwość samodzielnej aranżacji. To coś, na co warto czekać.</p>
                 <p>&nbsp;</p>
                 <p>Zastanawiasz się jednak, czy kupno mieszkania w centrum miasta to dobry pomysł? Zdecydowanie tak, jeśli zależy Ci na:</p>
@@ -479,16 +479,16 @@
     </div>
 </section>
 
-<section id="investor" class="dark-blue-bg">
+<section id="investor" class="dark-blue-bg rwd-section">
     <div class="container">
         <div class="row">
-            <div class="col-5 d-flex align-items-center">
+            <div class="col-12 col-lg-5 d-none d-lg-flex align-items-center mt-5 mt-lg-0">
                 <div>
                     <img src="{{ asset('/images/homepage-investor.jpg') }}" alt="" width="770" height="770">
                 </div>
             </div>
-            <div class="col-7 ps-5">
-                <div class="ps-5">
+            <div class="col-12 col-lg-7 ps-3 ps-lg-5">
+                <div class="ps-0 ps-lg-5">
                     <img src="{{ asset('/images/svg/header-deco.svg') }}" alt="" width="99" height="17" class="mb-3">
                     <h2>EURO-BUILD</h2>
                     <p><b>To polska firma deweloperska realizująca inwestycje budowlane i mieszkaniowe. Od ponad 20-lat z sukcesem realizuje kolejne projekty między innymi w Mławie, Trójmieście, Olsztynie kształtując otaczającą nas rzeczywistość.</b></p>
@@ -498,7 +498,7 @@
                     <p>Troska o środowisko naturalne to ważny element w strategii działania naszej firmy. EURO-BILD stara się optymalizować zużycie surowców, wykorzystuje nowe technologie i wdraża energooszczędne rozwiązania. Dba, by komfort codziennego życia zapewniały klientom nie tylko wygody luksusowego mieszkania, ale także całego terenu inwestycji.</p>
                     <p>&nbsp;</p>
                     <p>Spółka posiada stabilne zaplecze finansowe, a podwykonawców wybiera tylko sprawdzonych.</p>
-                    <a href="{{ route('contact.index') }}" class="bttn mt-5">Kontakt z nami</a>
+                    <a href="{{ route('contact.index') }}" class="bttn mt-4 mt-md-5">Kontakt z nami</a>
                 </div>
             </div>
         </div>
